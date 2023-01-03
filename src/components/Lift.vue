a6989121881f6461d09e4c18f88add3b659755c0
alvar91 <aleksey91scorp@bk.ru>
2023-01-03 15:12:09 +0300

    feat: lift layout

---
+++ src/components/Lift.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="shaft">
+    <div
+      :style="liftStyle"
+      :class="['cabin', { blink: state.isOpen }]"
+    >
+      <span v-if="state.isMoving" :class="[{ up: state.isMovingUp }]"> ↓ </span>
+      <span>{{ state.nextFloor }}</span>
+    </div>
+    <div v-for="floor in floors" :key="floor" class="cell"></div>
+  </div>
+</template>
