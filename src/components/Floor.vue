db0aa8b20931269b9c5156e485a8a16d50baa977
alvar91 <aleksey91scorp@bk.ru>
2023-01-03 15:09:42 +0300

    feat: floor layout

---
+++ src/components/Floor.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="floor">
+    <div class="button-container">
+      <span class="title">{{ number }}</span>
+      <button
+        @click="$emit('call', number)"
+        :class="['button', { activeButton: state.isRequested }]"
+      >
+        <span class="circle">
+          <span class="dot"></span>
+        </span>
+      </button>
+    </div>
+  </div>
+</template>
