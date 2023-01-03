8a85bcdd5edef4231be061142d1d54f263c85c79
alvar91 <aleksey91scorp@bk.ru>
2023-01-03 15:10:14 +0300

    feat: floor styles

--- src/components/Floor.vue
+++ src/components/Floor.vue
@@ -13,3 +13,67 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+.floor {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  align-items: flex-start;
+  justify-content: center;
+  padding-left: 20px;
+}
+
+.button-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: var(--gap);
+}
+
+.button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30px;
+  width: 30px;
+  border: 1px solid var(--primary-color);
+  border-radius: var(--border-radius);
+  background-color: var(--light-color);
+  cursor: pointer;
+}
+
+.circle {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 8px;
+  height: 8px;
+  border: 1px solid var(--primary-color);
+  border-radius: 50%;
+  padding: 2px;
+}
+
+.dot {
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+  background-color: var(--primary-color);
+}
+
+.activeButton {
+  border-color: var(--secondary-color);
+}
+
+.activeButton .circle {
+  border-color: var(--secondary-color);
+}
+
+.activeButton .dot {
+  background-color: var(--secondary-color);
+}
+
+.title {
+  font-weight: 600;
+}
+</style>
