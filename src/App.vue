556a9ad8f06d8d9e989791dd762364271e675bfe
alvar91 <aleksey91scorp@bk.ru>
2023-01-03 15:17:19 +0300

    feat: app styles

--- src/App.vue
+++ src/App.vue
@@ -26,3 +26,47 @@
     </div>
   </div>
 </template>
+
+
+<style>
+:root {
+  --primary-color: #00feff;
+  --secondary-color: #ef7317;
+  --light-color: #fff;
+  --dark-color: #000;
+  --grey-color: #e3e3e3;
+
+  --border-radius: 5px;
+  --xs-gap: 1px;
+  --gap: 10px;
+}
+
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: var(--dark-color);
+  box-sizing: border-box;
+  background-color: var(--grey-color);
+  user-select: none;
+}
+
+.layout {
+  padding: var(--gap);
+  height: 90vh;
+}
+
+.container {
+  display: flex;
+  margin-top: var(--gap);
+  gap: var(--xs-gap);
+  height: 100%;
+  flex-direction: row;
+  background-color: var(--grey-color);
+}
+
+.sub-container {
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column-reverse;
+}
+</style>
