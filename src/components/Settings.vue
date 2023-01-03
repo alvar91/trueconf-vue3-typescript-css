8acb52b16d399d829fb07f992a262bd31427453b
alvar91 <aleksey91scorp@bk.ru>
2023-01-03 15:14:13 +0300

    feat: settings styles

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -72,3 +72,67 @@
     </Transition>
   </Teleport>
 </template>
+
+<style scoped>
+.button {
+  padding: var(--gap);
+  border: none;
+  border-radius: var(--border-radius);
+  background-color: var(--secondary-color);
+  color: var(--light-color);
+  font-weight: 600;
+  cursor: pointer;
+}
+
+.button:disabled {
+  background-color: var(--grey-color);
+}
+
+.modal {
+  position: fixed;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  left: 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(0 0 0 / 0.3);
+  z-index: 10;
+  transition: opacity 0.2s linear;
+}
+
+.modal-container {
+  padding: var(--gap);
+  border-radius: var(--border-radius);
+  background-color: var(--light-color);
+}
+
+.modal-sub-container {
+  display: flex;
+  justify-content: space-between;
+  gap: var(--gap);
+  align-items: center;
+  margin-bottom: var(--gap);
+}
+
+.modal-title {
+  margin: 0;
+  font-size: 16px;
+  font-weight: 600;
+}
+
+.close-button {
+  height: 25px;
+  width: 25px;
+  border: none;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+}
+
+.controls {
+  display: flex;
+  gap: var(--gap);
+}
+</style>
