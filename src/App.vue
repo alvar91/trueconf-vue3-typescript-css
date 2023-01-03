ed5e69b21afbd1e85f72dbde750ff102a5fe85bf
alvar91 <aleksey91scorp@bk.ru>
2023-01-03 15:17:46 +0300

    feat: app scripts

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,89 @@
   </div>
 </template>
 
+<script setup lang="ts">
+import { ref, reactive, watchEffect, watch, onBeforeMount } from "vue";
+import Settings from "./components/Settings.vue";
+import Lift from "./components/Lift.vue";
+import Floor from "./components/Floor.vue";
+import { LiftI } from "./types/LiftI";
+import { FloorI } from "./types/FloorI";
+import { StateI } from "./types/StateI";
+
+const liftList = ref<LiftI[]>([]);
+const floorList = ref<FloorI[]>([]);
+
+const state = reactive<StateI>({
+  lifts: 1,
+  floors: 5,
+  queue: [],
+});
+
+onBeforeMount(() => {
+  const persistedJSON = localStorage.getItem("app");
+
+  if (persistedJSON !== null) {
+    const { lifts, floors, queue } = JSON.parse(persistedJSON);
+    state.lifts = lifts;
+    state.floors = floors;
+    state.queue = queue;
+  }
+});
+
+watchEffect(() => {
+  if (state.queue.length && liftList.value.some((lift) => lift.isIdle)) run();
+});
+
+watch(state, () => {
+  localStorage.setItem("app", JSON.stringify(state));
+});
+
+function changeSettings(floor: number, lift: number) {
+  localStorage.clear();
+
+  state.floors = floor;
+  state.lifts = lift;
+  state.queue.length = 0;
+
+  setTimeout(() => {
+    window.location.reload();
+  }, 100);
+}
+
+function call(floor: number) {
+  if (
+    !liftList.value.some((item) => item.currentFloor === floor && item.isIdle) &&
+    !liftList.value.some((item) => item.nextFloor === floor) &&
+    !state.queue.includes(floor)
+  ) {
+    const i = floorList.value.findIndex((item) => item.number === floor);
+    floorList.value[i].isRequested = true;
+    state.queue.push(floor);
+  }
+}
+
+function stop(floor: number) {
+  const i = floorList.value.findIndex((item) => item.number === floor);
+  floorList.value[i].isRequested = false;
+}
+
+function run() {
+  const freeLiftsArr = liftList.value.filter((item) => item.isIdle);
+
+  const floorsOfFreeElevsArr = freeLiftsArr.map((item) => item.currentFloor).sort((a, b) => b - a);
+
+  const closestFloor = floorsOfFreeElevsArr.reduce((a, b) =>
+    Math.abs(b - state.queue[0]) < Math.abs(a - state.queue[0]) ? b : a
+  );
+
+  const i = liftList.value.findIndex((item) => item.currentFloor === closestFloor && item.isIdle);
+
+  if (i !== -1 && state.queue.length > 0) {
+    // eslint-disable-next-line
+    liftList.value[i].nextFloor = state.queue.shift()!;
+  }
+}
+</script>
 
 <style>
 :root {
